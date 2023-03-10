bdd08e2ce6142907cf095c72663b1848573c4175
razmik <razmik91j@mail.ru>
2023-03-11 00:11:57 +0400

    test

--- src/pages/ExamplePageTwo.jsx
+++ src/pages/ExamplePageTwo.jsx
@@ -1,16 +1,15 @@
-import { Fragment } from "react";
-import { Seo } from "../components/Seo";
+import { Fragment, useLayoutEffect } from "react";
 
 export default function ExamplePageTwo() {
+  useLayoutEffect(() => {
+    document.title = "Learning how to add dynamic meta tags in react";
+    const metaImg = document.createElement("meta");
+    metaImg.setAttribute("og:images", "images/two.jpeg");
+    document.head.append(metaImg);
+  }, []);
+
   return (
     <Fragment>
-      <Seo
-        title="Learning React Helmet! Two"
-        description="Beginner friendly page for learning React Helmet. Page Two"
-        name="Company name."
-        type="article"
-        imgName="images/two.jpeg"
-      />
       <h1>Page Two</h1>
     </Fragment>
   );
