dccfeee35a5ebf6682995189a21545a7fcf283e3
razmik <razmik91j@mail.ru>
2023-03-11 01:04:02 +0400

    test

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,39 +1,36 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet-async";
 
-export function Seo(props) {
-  const [title, setTitle] = useState("My Website");
-  const [description, setDescription] = useState("This is my website.");
-  const [imageUrl, setImageUrl] = useState("https://example.com/image.jpg");
-  const [url, setUrl] = useState("https://example.com");
+class Seo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Dynamic Title",
+      description: "Dynamic Description",
+      url: window.location,
+      imageUrl: window.origin + "/images/two.jpeg",
+    };
+  }
 
-  // Update meta tags based on component props
-  useEffect(() => {
-    // eslint-disable-next-line react/prop-types
-    setTitle(props.title);
-    // eslint-disable-next-line react/prop-types
-    setDescription(props.description);
-    // eslint-disable-next-line react/prop-types
-    setImageUrl(props.imageUrl);
-    // eslint-disable-next-line react/prop-types
-    setUrl(props.imageUrl);
-  }, [props]);
-
-  return (
-    <div>
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content={url} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={imageUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Helmet>
-      {/* Rest of component */}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>{this.state.title}</title>
+          <meta name="description" content={this.state.description} />
+          <meta property="og:title" content={this.state.title} />
+          <meta property="og:description" content={this.state.description} />
+          <meta property="og:image" content={this.state.imageUrl} />
+          <meta property="og:url" content={this.state.url} />
+          <meta name="twitter:title" content={this.state.title} />
+          <meta name="twitter:description" content={this.state.description} />
+          <meta name="twitter:image" content={this.state.imageUrl} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Helmet>
+        {/* Rest of component */}
+      </div>
+    );
+  }
 }
+
+export default Seo;
